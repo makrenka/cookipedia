0a51bddff2c0acd1fd743c0bab78a89a33c0c77b
makrenka <smokrenko79@gmail.com>
2025-12-31 09:17:53 +0300

    add backend

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const recipies = [
+    {
+      name: "Recipe 2",
+      description: "Recipe 2 description...",
+      nick: "cool-recipe1",
+    },
+    {
+      name: "Recipe 1",
+      description: "Recipe 1 description...",
+      nick: "cool-recipe2",
+    },
+    {
+      name: "Recipe 3",
+      description: "Recipe 3 description...",
+      nick: "cool-recipe3",
+    },
+    {
+      name: "Recipe 4",
+      description: "Recipe 4 description...",
+      nick: "cool-recipe4",
+    },
+    {
+      name: "Recipe 5",
+      description: "Recipe 5 description...",
+      nick: "cool-recipe5",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <h1>Cookipedia</h1>
+      {recipies.map((recipe) => {
+        return (
+          <div key={recipe.nick}>
+            <h2>{recipe.name}</h2>
+            <p>{recipe.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
